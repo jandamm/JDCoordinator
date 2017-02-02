a4adccccf0a7f779fee95f2d1c67daa051f0a1e9
Jan Damm <jandammshaeuser@gmail.com>
2017-02-02 14:32:32 +0100

    Added parentCoordinators

--- JDCoordinator/JDCoordinator.swift
+++ JDCoordinator/JDCoordinator.swift
@@ -14,6 +14,21 @@ open class JDCoordinator: NSObject, JDCoordinatorProtocol {
     
     /// Returns direct parentCoordinator
     public internal(set) var parentCoordinator: JDParentCoordinator?
+    
+    /// Returns every parentCoordinator.
+    /// 
+    /// .first is .parentCoordinator. .last is uppermost Coordinator in stack (AppCoordinator in a default setup).
+    public var parentCoordinators: [JDParentCoordinator] {
+        var coords: [JDParentCoordinator] = []
+        var coord = self
+
+        while let parent = coord.parentCoordinator {
+            coords.append(parent)
+            coord = parent
+        }
+
+        return coords
+    }
 
     /// This navigationController pushes all ViewControllers
     public var navigationController: UINavigationController {
