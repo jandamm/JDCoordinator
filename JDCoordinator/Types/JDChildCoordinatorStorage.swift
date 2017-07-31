ee7edc8892cf561cb4489d1946f6f50c6aad1c05
Jan Damm <jandammshaeuser@gmail.com>
2017-07-31 11:51:26 +0200

    Refactor JDChildCoordinatorStorage

--- JDCoordinator/Types/JDChildCoordinatorStorage.swift
+++ JDCoordinator/Types/JDChildCoordinatorStorage.swift
@@ -10,28 +10,32 @@ import Foundation
 // TODO: - This has to be improved before release.
 /// Quick and temporary solution to replace the store JDChildCoordinators of an Array with a Set
 public struct JDChildCoordinatorStorage {
-    private var store: Set<AnyHashable> = []
+    private var store: Set<NSObject> = []
+
+    var count: Int {
+        return store.count
+    }
 
     mutating func add(_ coordinator: JDChildCoordinatorClass) {
-        self.store.insert(coordinator as! AnyHashable)
+        store.insert(coordinator as NSObject)
     }
 
     mutating func remove(_ coordinator: JDChildCoordinatorClass) {
-        store.remove(coordinator as! AnyHashable)
+        store.remove(coordinator as NSObject)
     }
 
     mutating func removeAll() {
         store.removeAll()
     }
-    
+
     func subtracting(_ coordinators: [JDChildCoordinatorClass]) -> [JDChildCoordinatorClass] {
-        let coordinators = Set(coordinators as! [AnyHashable])
+        let coordinators = Set(coordinators as [NSObject])
         let subtraction = store.subtracting(coordinators)
-        
-        return Array(subtraction) as! [JDChildCoordinatorClass]
+
+        return Array(subtraction) as! [JDChildCoordinatorClass] // Only JDCoordinatorClass can be added
     }
 
     func contains(_ coordinator: JDChildCoordinatorClass) -> Bool {
-        return self.store.contains(coordinator as! AnyHashable)
+        return store.contains(coordinator as NSObject)
     }
 }
