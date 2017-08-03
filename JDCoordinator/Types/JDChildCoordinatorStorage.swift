ed122c9dd1a1e61a5ebe0fa506cba8caea1faa17
Jan Damm <jandammshaeuser@gmail.com>
2017-08-03 17:18:10 +0200

    Add SetAlgebra to JDChildCoordinatorStorage

--- JDCoordinator/Types/JDChildCoordinatorStorage.swift
+++ JDCoordinator/Types/JDChildCoordinatorStorage.swift
@@ -9,11 +9,21 @@ import Foundation
 
 // TODO: - This has to be improved before release.
 /// Quick and temporary solution to replace the store JDChildCoordinators of an Array with a Set
-public struct JDChildCoordinatorStorage: Collection, Equatable, ExpressibleByArrayLiteral {
-    private var storage: Set<NSObject>
+public struct JDChildCoordinatorStorage: SetAlgebra, Collection, Equatable, ExpressibleByArrayLiteral {
 
+    // MARK: - Internal Storage
+    private typealias StorageElement = NSObject
+    private typealias Storage = Set<StorageElement>
+
+    private var storage: Storage
+
+    // MARK: - Initializers
     public init() {
         storage = []
+    }
+
+    private init(storage: Storage) {
+        self.storage = storage
     }
 
     // MARK: - Sequence
@@ -42,43 +52,78 @@ public struct JDChildCoordinatorStorage: Collection, Equatable, ExpressibleByArr
     }
 
     public subscript(position: Index) -> Element {
-        return storage[position] as! JDChildCoordinatorClass
+        return storage[position] as! Element
     }
 
     // MARK: - ExpressibleByArrayLiteral
-    public init(arrayLiteral elements: JDChildCoordinatorStorage.Element...) {
-        storage = Set(elements as [NSObject])
+    public init(arrayLiteral elements: Element...) {
+        storage = Set(elements as [StorageElement])
     }
 
     // MARK: - Equatable
     public static func ==(lhs: JDChildCoordinatorStorage, rhs: JDChildCoordinatorStorage) -> Bool {
         return lhs.storage == rhs.storage
     }
+
+    // MARK: - SetAlgebra
+    public var isEmpty: Bool {
+        return storage.isEmpty
+    }
+
+    public func symmetricDifference(_ other: JDChildCoordinatorStorage) -> JDChildCoordinatorStorage {
+        let difference = storage.symmetricDifference(other.storage)
+        return JDChildCoordinatorStorage(storage: difference)
+    }
+
+    @discardableResult public mutating func update(with newMember: Element) -> Element? {
+        return storage.update(with: newMember as StorageElement) as? Element
+    }
+
+    public mutating func formUnion(_ other: JDChildCoordinatorStorage) {
+        storage.formUnion(other.storage)
+    }
+
+    public mutating func formIntersection(_ other: JDChildCoordinatorStorage) {
+        storage.formIntersection(other.storage)
+    }
+
+    public mutating func formSymmetricDifference(_ other: JDChildCoordinatorStorage) {
+        storage.formSymmetricDifference(other.storage)
+    }
+
+    public func contains(_ element: Element) -> Bool {
+        return storage.contains(element as StorageElement)
+    }
+
+    @discardableResult public mutating func remove(_ member: Element) -> Element? {
+        return storage.remove(member as StorageElement) as? Element
+    }
+
+    @discardableResult public mutating func insert(_ newMember: Element) -> (inserted: Bool, memberAfterInsert: Element) {
+        return storage.insert(newMember as StorageElement) as! (Bool, Element)
+    }
+
+    public func union(_ other: JDChildCoordinatorStorage) -> JDChildCoordinatorStorage {
+        let union = storage.union(other.storage)
+        return JDChildCoordinatorStorage(storage: union)
+    }
+
+    public func intersection(_ other: JDChildCoordinatorStorage) -> JDChildCoordinatorStorage {
+        let intersection = storage.intersection(other.storage)
+        return JDChildCoordinatorStorage(storage: intersection)
+    }
 }
 
-// TODO: - Replace the following stuff
 public extension JDChildCoordinatorStorage {
-
-    public mutating func add(_ coordinator: JDChildCoordinatorClass) {
-        storage.insert(coordinator as NSObject)
-    }
-
-    public mutating func remove(_ coordinator: JDChildCoordinatorClass) {
-        storage.remove(coordinator as NSObject)
-    }
 
     public mutating func removeAll() {
         storage.removeAll()
     }
 
-    public func subtracting(_ coordinators: [JDChildCoordinatorClass]) -> [JDChildCoordinatorClass] {
-        let coordinators = Set(coordinators as [NSObject])
-        let subtraction = storage.subtracting(coordinators)
+    public func subtracting(_ coordinators: [Element]) -> JDChildCoordinatorStorage {
+        let coordinators = Set(coordinators as [StorageElement])
+        let other = JDChildCoordinatorStorage(storage: coordinators)
 
-        return Array(subtraction) as! [JDChildCoordinatorClass] // Only JDCoordinatorClass can be added
-    }
-
-    public func contains(_ coordinator: JDChildCoordinatorClass) -> Bool {
-        return storage.contains(coordinator as NSObject)
+        return subtracting(other)
     }
 }
