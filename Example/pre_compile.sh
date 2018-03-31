4c3a0ab24852ba6625d7fe5f2eced02630aaae22
Jan Dammshäuser <jandammshaeuser@gmail.com>
2018-04-01 00:31:11 +0200

    Optimize pre compile script

--- Example/pre_compile.sh
+++ Example/pre_compile.sh
@@ -5,12 +5,8 @@ echo "Run Sourcery"
 sourcery --config ../..
 
 if [[ $? -eq 127 ]]; then
-	echo ""
-	echo "‼️---⚠️---‼️"
-	echo "Sourcery is not installed."
-	echo "$ brew bundle"
-	echo "‼️---⚠️---‼️"
-	echo ""
+	echo "warning: Sourcery is not installed."
+	echo "warning: install with $ brew bundle"
 fi
 
 echo "Run SwiftFormat"
@@ -18,10 +14,6 @@ echo "Run SwiftFormat"
 swiftformat ../.. --semicolons "never" --ranges "noSpace"
 
 if [[ $? -eq 127 ]]; then
-	echo ""
-	echo "‼️---⚠️---‼️"
-	echo "SwiftFormat is not installed."
-	echo "$ brew bundle"
-	echo "‼️---⚠️---‼️"
-	echo ""
+	echo "warning: SwiftFormat is not installed"
+	echo "warning: install with $ brew bundle"
 fi
