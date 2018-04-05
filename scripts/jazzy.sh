7ed4311affa6a1206b32a6ece8f19eafc85f2d0a
Jan Dammshäuser <jandammshaeuser@gmail.com>
2018-04-06 00:03:20 +0200

    Update jazzy.sh to set copyright end year

--- scripts/jazzy.sh
+++ scripts/jazzy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Set copyright end year to current year
+find ../ -type f -name '.jazzy.yaml' -exec sed -i '' -E "s/^copyright: \'(.*) 2016 - [0-9]{4}/copyright: \'\1 2016 - $(date +%Y)/" {} +
+
 jazzy
 
 if [[ `git status --porcelain` != '' ]]; then
